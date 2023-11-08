66bc41dae9146207a7e856698341a5c171b2da14
ericaly <ely4@uic.edu>
2023-11-08 14:19:08 -0600

    Added Home Page notification functionality so that you can see notifcations and delete them -- 11/08/2023

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+  // Initialize notifications with a default list
+  const [notifications, setNotifications] = useState([
+    'Rent due in 5 days',
+    'Electricity bill due in 3 days',
+    'Netflix Subscription Due Tomorrow'
+  ]);
+
+  // Function to delete a notification
+  const deleteNotification = (indexToDelete) => {
+    setNotifications(notifications.filter((_, index) => index !== indexToDelete));
+  };
+
   return (
     <div>
       <h2>Welcome to the MoneySaver!</h2>
-      {/* Notifications and other content will go here */}
+      <div>
+        <h3>Notifications</h3>
+        <ul>
+          {notifications.map((notification, index) => (
+            <li key={index}>
+              {notification}
+              {}
+              <button onClick={() => deleteNotification(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+        {notifications.length === 0 && <p>No notifications left!</p>}
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+
